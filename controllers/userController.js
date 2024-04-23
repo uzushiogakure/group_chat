924d7dff0f874faecf28bcf048191e4d0fc2309a
kemala4680 <kema20.fathan@gmail.com>
2024-04-24 04:52:00 +0700

    fix: adjust app, routes, & bin

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,35 +1,36 @@
-const {User} = require("../models");
-const { signToken } = require('../helpers/jwt');
-const { hashPassword, comparePassword } = require("../helpers/bcrypt")
+const { User } = require("../models");
+const { signToken } = require("../helpers/jwt");
+const { hashPassword, comparePassword } = require("../helpers/bcrypt");
 
 class UserController {
-
-    static async register (req, res,  next) {
-        try {
-            const {name, email, password } = req.body
-            await User.create( {name, email, password: hashPassword(password) }) 
-            res.status(201).json({message: "User has successfully created"})
-        } catch (error) {
-            next(error)
-            console.log(error);
-        }
+  static async register(req, res, next) {
+    try {
+      const { name, email, password } = req.body;
+      await User.create({ name, email, password: hashPassword(password) });
+      res.status(201).json({ message: "User has successfully created" });
+    } catch (error) {
+      next(error);
+      console.log(error);
     }
-    static async login (req, res, next) {
-       try {
-            const {email, password} = req.body
-            if(!email) throw {name: "EmailRequired"}
-            if(!password) throw {name: "PasswordRequired"}
-            const checkEmail = await User.findOne({where : {email}})
-            if(!checkEmail) throw {name: "InvalidLogin"}
-            const checkPassword = comparePassword(password, checkEmail.password)
-            if(!checkPassword) throw {name: "InvalidLogin"}
-            const payload = {id: checkEmail.id}
-            const access_token = signToken(payload)
-            // console.log(access_token);
-            res.status(200).json({message: "successfully login", access_token: access_token})
-       } catch (error) {
-            next(error)
-       }
+  }
+  static async login(req, res, next) {
+    try {
+      const { email, password } = req.body;
+      if (!email) throw { name: "EmailRequired" };
+      if (!password) throw { name: "PasswordRequired" };
+      const checkEmail = await User.findOne({ where: { email } });
+      if (!checkEmail) throw { name: "InvalidLogin" };
+      const checkPassword = comparePassword(password, checkEmail.password);
+      if (!checkPassword) throw { name: "InvalidLogin" };
+      const payload = { id: checkEmail.id };
+      const access_token = signToken(payload);
+      // console.log(access_token);
+      res
+        .status(200)
+        .json({ message: "successfully login", access_token: access_token });
+    } catch (error) {
+      next(error);
     }
+  }
 }
-module.exports = UserController
+module.exports = UserController;
