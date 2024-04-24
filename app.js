97c46c1d24d67b40c56e388f23c082482f5f9bc4
kemala4680 <kema20.fathan@gmail.com>
2024-04-24 13:17:23 +0700

    feat: socket server

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ if (process.env.NODE_ENV !== "production") {
 } //Install environment env
 
 const { Server } = require("socket.io");
+const { Chat } = require("./models");
 const { createServer } = require("http");
 const cors = require("cors");
 const express = require("express");
@@ -24,6 +25,20 @@ const io = new Server(httpServer, {
 
 io.on("connection", (socket) => {
   socket.emit("message", "Welcome " + socket.id);
+
+  socket.broadcast.emit("message", "A user has joined the chat");
+
+  socket.on("disconnect", () => {
+    io.emit("message", "A user has left the chat");
+  });
+
+  socket.on("message:new", async (body) => {
+    try {
+      await Chat.create(body);
+    } catch (error) {
+      throw error;
+    }
+  });
 });
 
 app.use("/", router);
