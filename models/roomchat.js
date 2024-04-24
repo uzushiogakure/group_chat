650df291dbb78571e98f2137e96fd382ef42781e
arthayogi <arthayo91@gmail.com>
2024-04-24 11:11:03 +0700

    asosiasi user-roomchat-room

--- models/roomchat.js
+++ models/roomchat.js
@@ -11,6 +11,8 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      RoomChat.hasMany(models.User)
+      RoomChat.hasMany(models.Room)
     }
   }
   RoomChat.init({
--- models/user.js
+++ models/user.js
@@ -11,6 +11,8 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      User.hasMany(models.Chat)
+      User.hasMany(models.RoomChat)
     }
   }
   User.init({
