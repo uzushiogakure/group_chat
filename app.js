d1410332877fe339e686b4c8b2748f51d9033bd4
kemala4680 <kema20.fathan@gmail.com>
2024-04-24 07:48:15 +0700

    feat: http socket server

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 } //Install environment env
 
+const { Server } = require("socket.io");
+const { createServer } = require("http");
 const cors = require("cors");
 const express = require("express");
 const app = express();
@@ -13,8 +15,19 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(require("morgan")("dev"));
 
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+io.on("connection", (socket) => {
+  socket.emit("message", "Welcome " + socket.id);
+});
+
 app.use("/", router);
 
 app.use(errorHandler);
 
-module.exports = app;
+module.exports = httpServer;
