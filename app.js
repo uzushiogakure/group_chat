b13018d94524ed20528c3f90556b97e0bd393c6c
arthayogi <arthayo91@gmail.com>
2024-04-24 10:49:10 +0700

    setup socket app.js

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 require("dotenv").config(); //Install environment env
+const { Server } = require('socket.io')
 const cors = require("cors");
 const express = require("express");
 const app = express();
+const port = 3000
 const router = require("./routes")
+const httpServer = createServer(app)
+const io = new Server(server) // link deploynya
 
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true }))
 app.use(cors());
 app.use(require("morgan")("dev"))
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+  });
+
 app.use("/", router);
+
+httpServer.listen(port, () => {
+    console.log(`server running at https://127.0.0.1/${port}`);
+  });
 
 module.exports = app;
 
