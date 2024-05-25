94242e93e8bc3d15b259562dfb7d3f1527361336
yehezkielt <yehezkieltatang1@gmail.com>
2024-05-25 16:49:17 +0700

    debug

--- models/room.js
+++ models/room.js
@@ -1,41 +1,41 @@
 "use strict";
 const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-  class Room extends Model {
-    static associate(models) {
-      Room.belongsToMany(models.User, {
-        through: models.Chat,
-      });
-      Room.hasMany(models.Chat);
+    class Room extends Model {
+        static associate(models) {
+            Room.belongsToMany(models.User, {
+                through: models.Chat,
+            });
+            Room.hasMany(models.Chat);
+        }
     }
-  }
-  Room.init(
-    {
-      title: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          isNull: {
-            msg: "Group Name is required",
-          },
-          notEmpty: {
-            msg: "Group Name is required",
-          },
+    Room.init(
+        {
+            title: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                validate: {
+                    notNull: {
+                        msg: "Group Name is required",
+                    },
+                    notEmpty: {
+                        msg: "Group Name is required",
+                    },
+                },
+            },
+            description: {
+                type: DataTypes.STRING,
+                defaultValue: "-",
+            },
+            imgUrl: {
+                type: DataTypes.TEXT,
+                defaultValue: "https://www.doccen.vn/icons/default-group-avatar.svg",
+            },
         },
-      },
-      description: {
-        type: DataTypes.STRING,
-        defaultValue: "-",
-      },
-      imgUrl: {
-        type: DataTypes.TEXT,
-        defaultValue: "https://www.doccen.vn/icons/default-group-avatar.svg",
-      },
-    },
-    {
-      sequelize,
-      modelName: "Room",
-    }
-  );
-  return Room;
+        {
+            sequelize,
+            modelName: "Room",
+        }
+    );
+    return Room;
 };
